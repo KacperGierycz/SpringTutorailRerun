c0a00a9c7b72f6e163a13e1a0ae004f0420f36c8
Kacper <kacpergierycz@gmail.com>
2021-05-27 14:16:04 +0200

    index initialize

--- demo/src/main/java/com/example/demo/Triangle.java
+++ demo/src/main/java/com/example/demo/Triangle.java
@@ -4,23 +4,39 @@ package com.example.demo;
 public class Triangle {
 
 	private String type;
+	private int hight;
+
+	public Triangle(int hight) {
+		this.hight=hight;
+	}
 	
 	public Triangle(String type) {
 		this.type=type;
 	}
 	
+	public Triangle(String type, int hight) {
+		this.type=type;
+		this.hight=hight;
+	}
+	
+
+
 	public void draw() {
 		
-		System.out.println(getType() + " Triangle drawn");
+		System.out.println(getType() + " Triangle drawn hight is: "+getHight());
 		
+	}
+	
+	public int getHight() {
+		return hight;
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
+//	public void setType(String type) {
+//		this.type = type;
+//	}
 	
 }
