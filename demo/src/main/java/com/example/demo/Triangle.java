0032f40e232d7363748d25baa821d06d2070a2fd
Kacper <kacpergierycz@gmail.com>
2021-05-28 09:09:43 +0200

    spring points in triangle initializing properties in xml on demand of triangle properties

--- demo/src/main/java/com/example/demo/Triangle.java
+++ demo/src/main/java/com/example/demo/Triangle.java
@@ -3,37 +3,83 @@ package com.example.demo;
 
 public class Triangle {
 
-	private String type;
-	private int hight;
-
-	public Triangle(int hight) {
-		this.hight=hight;
-	}
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	public Triangle(String type) {
-		this.type=type;
-	}
-	
-	public Triangle(String type, int hight) {
-		this.type=type;
-		this.hight=hight;
-	}
-	
-
-
 	public void draw() {
-		
-		System.out.println(getType() + " Triangle drawn hight is: "+getHight());
-		
-	}
-	
-	public int getHight() {
-		return hight;
+		System.out.println("Point A = "+getPointA().getX()+", "+getPointA().getY());
+		System.out.println("Point B = "+getPointB().getX()+", "+getPointB().getY());
+		System.out.println("Point C = "+getPointC().getX()+", "+getPointC().getY());
 	}
 
-	public String getType() {
-		return type;
+	public Point getPointA() {
+		return pointA;
 	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	private String type;
+//	private int hight;
+//
+//	public Triangle(int hight) {
+//		this.hight=hight;
+//	}
+//	
+//	public Triangle(String type) {
+//		this.type=type;
+//	}
+//	
+//	public Triangle(String type, int hight) {
+//		this.type=type;
+//		this.hight=hight;
+//	}
+	
+
+
+//	public void draw() {
+//		
+//		System.out.println(getType() + " Triangle drawn hight is: "+getHight());
+//		
+//	}
+	
+//	public int getHight() {
+//		return hight;
+//	}
+//
+//	public String getType() {
+//		return type;
+//	}
 
 //	public void setType(String type) {
 //		this.type = type;
